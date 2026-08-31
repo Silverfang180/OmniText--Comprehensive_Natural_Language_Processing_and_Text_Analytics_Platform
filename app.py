ed767e9058c7aeb75eed5762da851f3e56677146
Shaikh Faizal <shaikhfaizal24062004@gmail.com>
2026-08-31 17:16:21 +0530

    import app directly in app.py for uvicorn

--- app.py
+++ app.py
@@ -13,7 +13,9 @@ env["PYTHONPATH"] = api_dir + os.pathsep + env.get("PYTHONPATH", "")
 print("Launching async database worker subprocess...")
 worker_process = subprocess.Popen([sys.executable, "-m", "omnitext.worker.main"], env=env)
 
+from omnitext.main import app
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
     print(f"Starting FastAPI server on port {port}...")
-    uvicorn.run("omnitext.main:app", host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=port)
